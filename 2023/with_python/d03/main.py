1d189e7783e41d751721098d32fd71668613a833
Sid Mitra <sidmitra.del@gmail.com>
2023-12-29 09:10:25 -0300

    Added d03, empty d25

--- 2023/with_python/d03/main.py
+++ 2023/with_python/d03/main.py
@@ -1,3 +1,4 @@
+import sys
 import string
 import re
 from math import prod
@@ -7,12 +8,13 @@ from typing import TypedDict
 
 
 
-def parse_grid(filename):
-    with open(filename, 'r') as infile:
-        lines = infile.readlines()
+def strip_newlines(lines):
+    """
+    Return array of arrays
+    """
     grid = []
     for line in lines:
-        grid.append(line)
+        grid.append(line.strip())
     return grid
 
 """
@@ -29,21 +31,18 @@ def parse_grid(filename):
 """
 
 def is_symbol(grid, y, x):
+    # Check for bounds
     if y < 0 or x < 0:
         return False
     if y >= len(grid) or x >= len(grid[y]):
         return False
 
     value = grid[y][x]
-    if value.isnumeric() or value == "." or value == " ":
+    if value.isnumeric() or value == ".":
         return False
-    # print(value, x, y, grid[y][x])
     return True
 
 def has_symbol_neighbour(grid, y, x):
-    # print(grid[y][x], "-")
-
-    # import ipdb; ipdb.set_trace()
     up = is_symbol(grid, y-1, x)
     down = is_symbol(grid, y+1, x)
     left = is_symbol(grid, y, x-1)
@@ -57,26 +56,36 @@ def has_symbol_neighbour(grid, y, x):
     return has_symbol
 
 
-def first(filename):
-    grid = parse_grid(filename)
+def first(lines):
     numbers = []
     part_numbers = []
 
-    for y, line in enumerate(grid):
+    for y, row in enumerate(lines):
         current_num = ""
         is_part_number = False
-        for x, char in enumerate(line.strip()):
+        width = len(row)
+        for x, char in enumerate(row):
             if char.isnumeric():
                 current_num += char
                 # Look around, if there is a symbol, then we have a part numbers
-                if has_symbol_neighbour(grid, y, x):
+                if has_symbol_neighbour(lines, y, x):
                     is_part_number = True
+                # If the row ends with a number, ensure we add it to parts
+                # F*CK: This took a long time to figure out, had to look up hints on reddit
+                if x == width-1:
+                    numbers.append(int(current_num))
+                    if is_part_number:
+                        part_numbers.append(int(current_num))
+                    current_num = ""
+                    is_part_number = False
+
             else:
                 if current_num:
                     numbers.append(int(current_num))
                     if is_part_number:
                         part_numbers.append(int(current_num))
-                    #print(int(current_num))
+
+
                 # start search for a new number
                 current_num = ""
                 is_part_number = False
@@ -84,19 +93,135 @@ def first(filename):
         # break  # TODO: remove this
 
     # print(numbers)
+    # print(part_numbers)
+    print(sum((part_numbers)))
+
+
+# --------
+def has_two_part_numbers(lines, x, y):
+    part_numbers = [] # only for current line
+
+    current_line = lines[y]
+    # ------ left
+    ix = x
+    current_num = ""
+    for ix in range(x-1, -1, -1):
+        if ch:= current_line[ix]:
+            if ch.isnumeric():
+                current_num += ch
+            else:
+                break
+    if current_num:
+        current_num = int(current_num[::-1]) # reverse
+        part_numbers.append(current_num)
+        # print(current_num, "=", current_line)
+    # print(part_numbers)
+    # ------ left
+
+
+    # ------ right
+    current_num = ""
+    for ix in range(x+1, len(current_line), 1):
+        if ch:= current_line[ix]:
+            if ch.isnumeric():
+                current_num += ch
+            else:
+                break
+    if current_num:
+        current_num = int(current_num)
+        part_numbers.append(current_num)
+        # print(current_num, "=", current_line)
+    # ------ right
+
+
+    # ------ up
+    if y > 0:
+        line_above = lines[y-1]
+
+        right_num = ""
+        for ix in range(x+1, len(line_above), 1):
+            if ch:= line_above[ix]:
+                if ch.isnumeric():
+                    right_num += ch
+                else:
+                    break
+
+        left_num = ""
+        for ix in range(x-1, -1, -1):
+            if ch:= line_above[ix]:
+                if ch.isnumeric():
+                    left_num += ch
+                else:
+                    break
+
+        combined = left_num[::-1] + line_above[x] + right_num
+        combined = [el for el in combined.strip(".").split(".") if el]
+        if combined:
+            combined = [int(x) for x in combined]
+            part_numbers.extend(combined)
+    # ------ up
+
+
+    # ------- down
+    if y < len(lines)-1:
+        line_below = lines[y+1]
+
+        right_num = ""
+        for ix in range(x+1, len(line_below), 1):
+            if ch:= line_below[ix]:
+                if ch.isnumeric():
+                    right_num += ch
+                else:
+                    break
+
+        left_num = ""
+        for ix in range(x-1, -1, -1):
+            if ch:= line_below[ix]:
+                if ch.isnumeric():
+                    left_num += ch
+                else:
+                    break
+
+        combined = left_num[::-1] + line_below[x] + right_num
+        combined = [el for el in combined.strip(".").split(".") if el]
+        if combined:
+            combined = [int(x) for x in combined]
+            part_numbers.extend(combined)
+    # -------- down
+
+
     print(part_numbers)
-    print(sum(part_numbers))
+    if len(part_numbers) == 2:
+        return prod(part_numbers)
+    return None
 
 
-# def second(filename):
-#     grid = parse_grid(filename)
-#     numbers = []
-#     part_numbers = []
+
+def second(lines):
+    """
+    A gear is any * symbol that is adjacent to exactly two part numbers. Its gear ratio is the result of multiplying those two numbers together.
+    """
+    gear_ratios = []
+    for y, row in enumerate(lines):
+        for x, char in enumerate(row):
+            if char == "*":
+                if gear_ratio := has_two_part_numbers(lines, x, y):
+                    gear_ratios.append(gear_ratio)
+
+    print("Done.")
+    #print(gear_ratios)
+    print(sum(gear_ratios))
 
 
+# python main.py input1.txt
 if __name__ == "__main__":
-    #first("input1_test.txt")
-    first("input1.txt")
+    filename = sys.argv[1]
+    with open(filename, "r") as infile:
+        lines = infile.readlines()
+    lines = strip_newlines(lines)
+
+    #first(lines)
     # 529539 - too low
-    # second("input1_test.txt")
-    #second("input1.txt")
+    # 332836 - incorrect
+
+    second(lines)
--- 2023/with_python/d11/main.py
+++ 2023/with_python/d11/main.py
@@ -1,9 +1,53 @@
 import sys
+from copy import copy, deepcopy
+
+
+def print_grid(grid):
+    for row in grid:
+        for col in row:
+            print(col, end="")
+        print("\n")
+
+
+def expand_rows(grid, rows_without_galaxies):
+    for row in rows_without_galaxies:
+        grid.insert(row, deepcopy(grid[row]))
+
+
+def expand_cols(grid, cols_without_galaxies):
+    for col in cols_without_galaxies:
+        for row in grid:
+            row.insert(col, ".")
 
 
 
 def first(lines):
-    pass
+    grid = []
+    for line in lines:
+        grid.append(list(line.strip()))
+    # print_grid(grid)
+
+    rows_without_galaxies = []
+    for r, row in enumerate(grid):
+        if "#" not in row:
+            rows_without_galaxies.append(r)
+    # print(rows_without_galaxies)
+
+    cols_without_galaxies = []
+    for col in range(len(grid)):
+        if all(grid[y][col]=="." for y in range(len(lines))):
+            cols_without_galaxies.append(col)
+    # print(cols_without_galaxies)
+
+    # double rows
+    expand_rows(grid, rows_without_galaxies)
+    # print_grid(grid)
+
+    # double cols
+    expand_cols(grid, cols_without_galaxies)
+    print_grid(grid)
+
+
 
 
 
