ac0541b95b7ab4b1a0cf4b4ed149770f421c23f1
Sid Mitra <sidmitra.del@gmail.com>
2024-01-04 22:21:02 -0300

    wip: d25

--- 2023/with_python/d25/main.py
+++ 2023/with_python/d25/main.py
@@ -1,12 +1,16 @@
 import sys
 
 
+def first(lines):
+    print(lines)
+
+
+
 # python main.py input.txt
 if __name__ == "__main__":
     filename = sys.argv[1]
     with open(filename, "r") as infile:
-        lines = infile.readlines()
-    lines = strip_newlines(lines)
+        lines = infile.read().splitlines()
 
     first(lines)
 
