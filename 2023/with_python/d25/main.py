86b3b4b89ec59e31d4f96c4cf336fe963569f45e
Sid Mitra <sidmitra.del@gmail.com>
2024-01-07 21:50:07 -0300

    Add d25

--- 2023/with_python/d25/main.py
+++ 2023/with_python/d25/main.py
@@ -1,8 +1,31 @@
+import math
 import sys
+import networkx as nx
+import matplotlib.pyplot as plt
 
+def first(lines, stoer_wagner=False):
+    G = nx.Graph() # non directional graph
+    for line in lines:
+        src, dests = line.strip().split(":")
+        dests = dests.strip().split(" ")
 
-def first(lines):
-    print(lines)
+        for dest in dests:
+            G.add_edge(src, dest)
+
+    if not stoer_wagner:
+        edges_to_cut = nx.minimum_edge_cut(G)
+        G.remove_edges_from(edges_to_cut)
+
+        # nx.draw(G, with_labels=True)
+        # plt.show()
+        # After removing the edges, we need to count the connected components and multiply them
+        components = nx.connected_components(G)
+        print(math.prod([len(c) for c in components]))
+
+    if stoer_wagner:
+        # can also be done with with stoer wagner
+        cut_value, partition = nx.stoer_wagner(G)
+        print("Prod: ", math.prod([len(p) for p in partition]))
 
 
 
@@ -12,6 +35,6 @@ if __name__ == "__main__":
     with open(filename, "r") as infile:
         lines = infile.read().splitlines()
 
-    first(lines)
+    first(lines, stoer_wagner=True)
 
     # second(lines)
